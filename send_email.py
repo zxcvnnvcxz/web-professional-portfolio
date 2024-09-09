d070f3806a489a2867ca5f3941390ed17776cd9f
EDWARD <edward.kang0301@gmail.com>
2024-09-09 21:27:44 +1000

    Added environment pw

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+import os
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "edward.kang0301@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    receiver = "edward.kang0301@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
