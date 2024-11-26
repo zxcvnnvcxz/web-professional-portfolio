cad7e92a19989239dbc55cee1664b13f7e66ccf2
EDWARD <edward.kang0301@gmail.com>
2024-11-27 05:24:50 +1100

    Formatted website layout

--- Home.py
+++ Home.py
@@ -1,22 +1,31 @@
 import streamlit as st
 import pandas
+import os
 
 st.set_page_config(layout='wide')
+image_path = os.path.join(os.getcwd(), "images/photo.png")
+left_col, cent_col, last_col = st.columns(3)
+with cent_col:
+    st.image(image_path)
 
-col1, col2 = st.columns(2)
+with st.container():
+    # Use HTML to center both the image and the text
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <h1>Edward Kang</h1>
+            <p>
+                Hi, I am ! I've worked as a dynamic IT Support Technician with experience in event translation, 
+                eager to return to work after a year of exchange abroad in Japan.
+                Below are some of the apps I have built in Python.
+            </p>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
 
-with col1:
-    st.image("images/photo.png")
-
-with col2:
-    st.title("Edward Kang")
-    content = """Hi, I am Edward! I've worked as a dynamic IT Support Technician with experience in event translation, 
-    eager to return to work after a year of exchange abroad in Japan."""
-    st.info(content)
-
-intro = """Below are some of the apps I have built in Python."""
+intro = ""
 st.write(intro)
-
 col3, empty_col, col4 = st.columns([1.5, 0.3, 1.5]) # Gap between the columns
 
 df = pandas.read_csv("data.csv", sep=";")
